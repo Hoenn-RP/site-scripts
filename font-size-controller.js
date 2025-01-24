d26e0538e4969d1bf341623a188bd1e14b1a4c82
Hoenn-RP <159826511+Hoenn-RP@users.noreply.github.com>
2025-01-24 16:02:50 -0500

    Add files via upload

---
+++ font-size-controller.js
@@ -0,0 +1,31 @@
+(function ($) {
+    "use strict";
+    function \Size() {
+        let fontBtn = $(".fontbtn");
+        if (localStorage.getItem("fontSize") === null) {
+            var htmlFontSize = 12; // THIS IS YOUR DEFAULT FONT SIZE
+        } else {
+            var htmlFontSize = localStorage.getItem("fontSize");
+            $(".palise-post-right").css("font-size", htmlFontSize + "px"); // GENERIC POST CONTAINER
+        }
+        fontBtn.click(function (e) {
+            e.preventDefault();
+            let $this = $(this);
+            if ($this.hasClass("fontinc")) {
+                htmlFontSize = Math.round(parseFloat(htmlFontSize) + 1);
+                $(".palise-post-right").css("font-size", htmlFontSize + "px");
+            } else {
+                htmlFontSize = Math.round(parseFloat(htmlFontSize) - 1);
+                $(".palise-post-right").css("font-size", htmlFontSize + "px");
+            }
+            if ($this.hasClass("fontres")) {
+                htmlFontSize = 12;
+                $(".palise-post-right").css("font-size", htmlFontSize + "px");
+            }
+            localStorage.setItem("fontSize", htmlFontSize);
+        });
+    }
+    $(window).on("load", function () {
+        fontSize();
+    });
+})(jQuery);
