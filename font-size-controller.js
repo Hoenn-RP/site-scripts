2684fc06d037f386a82f1c13052210b56eeadc97
Hoenn-RP <159826511+Hoenn-RP@users.noreply.github.com>
2025-04-05 19:54:33 -0400

    Update font-size-controller.js

--- font-size-controller.js
+++ font-size-controller.js
@@ -6,21 +6,21 @@
             var htmlFontSize = 12; // THIS IS YOUR DEFAULT FONT SIZE
         } else {
             var htmlFontSize = localStorage.getItem("fontSize");
-            $(".palise-post-right").css("font-size", htmlFontSize + "px"); // GENERIC POST CONTAINER
+            $(".posts .post .content").css("font-size", htmlFontSize + "px"); // GENERIC POST CONTAINER
         }
         fontBtn.click(function (e) {
             e.preventDefault();
             let $this = $(this);
             if ($this.hasClass("fontinc")) {
                 htmlFontSize = Math.round(parseFloat(htmlFontSize) + 1);
-                $(".palise-post-right").css("font-size", htmlFontSize + "px");
+                $(".posts .post .content").css("font-size", htmlFontSize + "px");
             } else {
                 htmlFontSize = Math.round(parseFloat(htmlFontSize) - 1);
-                $(".palise-post-right").css("font-size", htmlFontSize + "px");
+                $(".posts .post .content").css("font-size", htmlFontSize + "px");
             }
             if ($this.hasClass("fontres")) {
                 htmlFontSize = 12;
-                $(".palise-post-right").css("font-size", htmlFontSize + "px");
+                $(".posts .post .content").css("font-size", htmlFontSize + "px");
             }
             localStorage.setItem("fontSize", htmlFontSize);
         });
