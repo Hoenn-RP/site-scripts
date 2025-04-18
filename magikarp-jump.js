11d0c2199b6103c625cc7e3f7f15c49c9334ffa5
Hoenn-RP <159826511+Hoenn-RP@users.noreply.github.com>
2025-04-18 16:51:05 -0400

    Update magikarp-jump.js

--- magikarp-jump.js
+++ magikarp-jump.js
@@ -1,5 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-
 function runMagikarpJumpLogic() {
     $('.magikarp-jump-1 .vdice-value').each(function () {
         const rollText = $(this).text().trim();
@@ -10,93 +8,93 @@ function runMagikarpJumpLogic() {
         let resultText = '';
 
         if (roll >= 1 && roll <= 5) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">The Magikarp appears to be having a good time when it uses <b>Celebrate</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 6 && roll <= 10) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">The Magikarp fires coins from the water while using <b>Happy Hour</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 11 && roll <= 20) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">The Magikarp flops violently and somehow unleashes <b>Incinerate</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 21 && roll <= 30) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">The Magikarp charges up and lets loose a wild <b>Zap Cannon</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 31 && roll <= 40) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">The Magikarp locks eyes with you and uses <b>Mimic</b>.</div>
-                </div>`;
+                </div>;
         } else if (roll >= 41 && roll <= 50) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">Dark energy swirls around the Magikarp as it uses <b>Curse</b>.</div>
-                </div>`;
+                </div>;
         } else if (roll >= 51 && roll <= 60) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">The Magikarp tucks in and launches itself with <b>Skull Bash</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 61 && roll <= 70) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">A surge of water explodes from the Magikarp when it uses <b>Brine</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 71 && roll <= 80) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">Ice builds around the Magikarp as it uses <b>Avalanche</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 81 && roll <= 90) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">A shimmering barrier appears around the Magikarp as it casts <b>Reflect</b>.</div>
-                </div>`;
+                </div>;
         } else if (roll >= 91 && roll <= 100) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">The Magikarp flips and counters with <b>Reversal</b>!</div>
-                </div>`;
+                </div>;
         } else {
             resultText = '<div class="mkj-wrapper"><div class="mkj-textbox">Something went wrong with the roll...</div></div>';
         }
@@ -115,69 +113,69 @@ function runMagikarpJumpLogic() {
         let resultText = '';
 
         if (roll >= 1 && roll <= 13) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">That’s a tiny one! It seems <b>Rattled</b>. This Magikarp weighs only <b>5.0</b> kg <b>(XXXS)</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 14 && roll <= 26) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">This small Magikarp is going for a <b>Swift Swim</b>. It only weighs <b>7.5</b> kg <b>(XXS)</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 27 && roll <= 39) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">Appears to be slightly small during its <b>Swift Swim</b>. It only weighs <b>9.0</b> kg <b>(XS)</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 40 && roll <= 52) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">A standard catch! This one’s rocking <b>Swift Swim</b> and weighs <b>10.0</b> kg <b>(Normal)</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 53 && roll <= 65) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">Pretty hefty! This <b>Swift Swim</b> Magikarp weighs <b>11.0</b> kg <b>(XL)</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 66 && roll <= 78) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">It’s heavy! With its <b>Swift Swim</b> ability, this Magikarp weighs <b>12.5</b> kg <b>(XXL)</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 79 && roll <= 90) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">Holy smokes! It’s huge! A whopping <b>15.0</b> kg <b>(XXXL)</b> and clearly <b>Rattled</b>!</div>
-                </div>`;
+                </div>;
         } else if (roll >= 91 && roll <= 100) {
-            resultText = `
+            resultText = 
                 <div class="mkj-wrapper">
                     <div class="mkj-magikarp">
                         <img src="https://file.garden/Zz6LAVCcyQejC5CU/Events/Magikarp/Magikarp_Jump_Pattern_01.png">
                     </div>
                     <div class="mkj-textbox">It’s an <b>Alpha</b> Magikarp radiating power! It has the ability <b>Wild Might</b> and weighs <b>22.5</b> kg <b>(ALPHA)</b>!</div>
-                </div>`;
+                </div>;
         } else {
             resultText = 'Something went wrong with the roll...';
         }
@@ -265,6 +263,6 @@ function runMagikarpJumpLogic() {
     });
 }
 
-    document.addEventListener('DOMContentLoaded', runMagikarpJumpLogic);
+$(document).ready(function () {
+    runMagikarpJumpLogic();
 });
-
