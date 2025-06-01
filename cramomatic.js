464866f38341c2e7dfde544f661653c71f8ac07f
Hoenn-RP <159826511+Hoenn-RP@users.noreply.github.com>
2025-06-01 05:45:21 -0400

    Add files via upload

---
+++ cramomatic.js
@@ -0,0 +1,116 @@
+function CramomaticDice() {
+  $(document).ready(function () {
+    $('.cramomatic-01 .vdice-value').each(function () {
+      const rollText = $(this).text().trim();
+      const roll = parseInt(rollText, 10);
+
+      if (isNaN(roll)) return;
+
+      let resultText = '';
+
+      if (roll >= 1 && roll <= 10) {
+        resultText = `
+        <img src="https://i.imgur.com/bwjRsaA.png" title="shiny swap">
+        <div class="cramomatic-desc">The Cram-o-matic coughed up a Shiny Swap!</div>`;
+      } else if (roll >= 11 && roll <= 20) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/incense/full.png" title="shiny incense">
+        <div class="cramomatic-desc">A Shiny Incense blows smoke as it exits the machine!</div>`;
+      } else if (roll >= 21 && roll <= 30) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/ball/heavy.png" title="strange mineral">
+        <div class="cramomatic-desc">A Strange Mineral clanked out of the chute!</div>`;
+      } else if (roll >= 31 && roll <= 40) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/evo-item/up-grade.png" title="disc of reuse">
+        <div class="cramomatic-desc">A Disk of Reuse fired out from the machine's beak!</div>`;
+      } else if (roll >= 41 && roll <= 45) {
+        resultText = `
+        <img src="https://file.garden/ZXZrnWMYuz15frfJ/SPRITES/safari%20ticket.png" title="safari ticket">
+        <div class="cramomatic-desc">A Safari Ticket fluttered out like a feather.</div>`;
+      } else if (roll >= 46 && roll <= 50) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/mega-stone/abomasite.png" title="mega stone">
+        <div class="cramomatic-desc">The beak popped open with a Mega Stone!</div>`;
+      } else if (roll >= 51 && roll <= 55) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/z-crystals/waterium-z--held.png" title="z-crystal">
+        <div class="cramomatic-desc">A Z-Crystal gleamed as it dropped from the machine's beak.</div>`;
+      } else if (roll >= 56 && roll <= 60) {
+        resultText = `
+        <img src="https://file.garden/Zz6LAVCcyQejC5CU/Hoenn/Item%20Sprites/stellar%20tera%20shard%20sprite%20hoenn.png" title="tera shard">
+        <div class="cramomatic-desc">A Tera Shard launched with a squawk!</div>`;
+      } else if (roll >= 61 && roll <= 65) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/fossil/helix.png" title="fossil">
+        <div class="cramomatic-desc">A dusty Fossil clattered onto the tray!</div>`;
+      } else if (roll >= 66 && roll <= 70) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/exp-candy/xl.png" title="legendary candy">
+        <div class="cramomatic-desc">The Cram-o-matic shook and dropped a Legendary Candy!</div>`;
+      } else if (roll >= 71 && roll <= 75) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/plate/iron.png" title="plate of your choice">
+        <div class="cramomatic-desc">A mysterious Plate gently slid out!</div>`;
+      } else if (roll >= 76 && roll <= 80) {
+        resultText = `
+        <img src="https://file.garden/Zz6LAVCcyQejC5CU/Hoenn/Item%20Sprites/mythical%20pecha%20berry.png" title="mythical pecha berry">
+        <div class="cramomatic-desc">A Mythical Pecha Berry plopped onto the ground!</div>`;
+      } else if (roll >= 81 && roll <= 85) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/hold-item/black-sludge.png" title="shadow sludge">
+        <div class="cramomatic-desc">Shadow Sludge oozed out of the Cram-o-matic with a burp!</div>`;
+      } else if (roll >= 86 && roll <= 90) {
+        resultText = `
+        <img src="https://file.garden/ZXZrnWMYuz15frfJ/SPRITES/crystabloomitemsprite%20by%20king.png" title="crystalbloom">
+        <div class="cramomatic-desc">A glowing Crystalbloom blasted out of the Cram-o-matic's throat!</div>`;
+      } else if (roll >= 91 && roll <= 92) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/berry/occa.png" title="occa berry">
+        <div class="cramomatic-desc">An Occa Berry plopped onto the ground!</div>`;
+      } else if (roll >= 93 && roll <= 94) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/key-item/mysticticket.png" title="warden's ticket">
+        <div class="cramomatic-desc">A Warden's Ticket fluttered out like a feather!</div>`;
+      } else if (roll >= 95 && roll <= 96) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/valuable-item/comet-shard.png" title="three gachapon tickets">
+        <div class="cramomatic-desc">Three gachapon tickets sprayed out like rapid-fire!</div>`;
+      } else if (roll === 97) {
+        resultText = `
+        <img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/key-item/rule-book.png" title="extra move slot">
+        <div class="cramomatic-desc">The machined whirrled and dropped and Extra Move Slot!</div>`;
+      } else if (roll === 98) {
+        resultText = `
+        <img src="https://i.imgur.com/oNiM0nd.png" title="three premium gachapon tickets">
+        <div class="cramomatic-desc">Three premium gachapon tickets sprayed out like rapid-fire!</div>`;
+      } else if (roll === 99) {
+        resultText = `
+        <img src="https://file.garden/Zz6LAVCcyQejC5CU/Hoenn/Item%20Sprites/custompokemonticket.png" title="custom pokemon ticket">
+        <div class="cramomatic-desc">A Custom Pokemon Ticket fluttered out like a feather!</div>`;
+      } else if (roll === 100) {
+        resultText = `
+        <img src="https://file.garden/Zz6LAVCcyQejC5CU/Hoenn/Item%20Sprites/cramorant.png" title="crammed! award">
+        <div class="cramomatic-desc">The Cram-o-matic chirped and released treasure from its mouth!</div>`;
+      } else {
+        resultText = `<img src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/ball/poke.png" title="what went wrong?">
+        <div class="cramomatic-desc">Something went wrong with the roll...</div>`;
+      }
+
+      $(this).html(resultText);
+    });
+  });
+}
+
+$(document).ready(function () {
+  CramomaticDice();
+
+  const target = document.querySelector('.content');
+  if (target) {
+    const observer = new MutationObserver(() => {
+      CramomaticDice();
+    });
+
+    observer.observe(target, { childList: true, subtree: true });
+  }
+});
