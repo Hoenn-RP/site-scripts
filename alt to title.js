a5e24403bd4769a103ae0f2be54cd1c8cec92ea3
Hoenn-RP <159826511+Hoenn-RP@users.noreply.github.com>
2025-07-03 21:45:31 -0400

    Delete alt to title.js

--- alt to title.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("img[alt]").forEach(img => {
-    if (!img.hasAttribute("title")) {
-      img.setAttribute("title", img.getAttribute("alt"));
-    }
-  });
-});
