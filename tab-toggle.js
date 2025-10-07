7b5acf8ea4a39a432aa0e2dc5e7f4e8817ef4cbb
Hoenn-RP <159826511+Hoenn-RP@users.noreply.github.com>
2025-10-07 03:26:47 -0400

    Update tab-toggle.js

--- tab-toggle.js
+++ tab-toggle.js
@@ -1,135 +1,70 @@
-$(".mini, .closetabs").on("click", function () {
-    $(this).parents("#mini-out").children(".mini").removeClass('clicked');
-    $(this).parents("#mini-out").children(".tab").removeClass('show');
-    $(this).parents("#mini-out").children(".closetabs").addClass("remove");
-    $(this).parents("#mini-out").children(".palise-mini-circ").addClass("small");
-    $(this).parents("#mini-out").children(".palise-mini-grad").addClass("fade");
-});
-
-$(".mini1").on("click", function () {
-    $(this).parents("#mini-out").children(".mini1").addClass('clicked');
-    $(this).parents("#mini-out").children(".tab1").addClass('show');
-    $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-    $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-    $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-});
-$(".mini2").on("click", function () {
-    $(this).parents("#mini-out").children(".mini2").addClass('clicked');
-    $(this).parents("#mini-out").children(".tab2").addClass('show');
-    $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-    $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-    $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-});
-$(".mini3").on("click", function () {
-    $(this).parents("#mini-out").children(".mini3").addClass('clicked');
-    $(this).parents("#mini-out").children(".tab3").addClass('show');
-    $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-    $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-    $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-});
-$(".mini4").on("click", function () {
-    $(this).parents("#mini-out").children(".mini4").addClass('clicked');
-    $(this).parents("#mini-out").children(".tab4").addClass('show');
-    $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-    $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-    $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-});
-$(".mini5").on("click", function () {
-    $(this).parents("#mini-out").children(".mini5").addClass('clicked');
-    $(this).parents("#mini-out").children(".tab5").addClass('show');
-    $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-    $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-    $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-});
-$(".mini6").on("click", function () {
-    $(this).parents("#mini-out").children(".mini6").addClass('clicked');
-    $(this).parents("#mini-out").children(".tab6").addClass('show');
-    $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-    $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-    $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-});
-
 $ = jQuery.noConflict();
-$(document).ajaxComplete(function () {
-    $(".mini, .closetabs").on("click", function () {
-        $(this).parents("#mini-out").children(".mini").removeClass('clicked');
-        $(this).parents("#mini-out").children(".tab").removeClass('show');
-        $(this).parents("#mini-out").children(".closetabs").addClass("remove");
-        $(this).parents("#mini-out").children(".palise-mini-circ").addClass("small");
-        $(this).parents("#mini-out").children(".palise-mini-grad").addClass("fade");
+
+function initMiniTabs() {
+    // Close all tabs when clicking a mini or closetabs
+    $("#mini-out").off('click', '.mini, .closetabs').on('click', '.mini, .closetabs', function () {
+        const $parent = $(this).parents("#mini-out");
+        $parent.children(".mini").removeClass('clicked');
+        $parent.children(".tab").removeClass('show');
+        $parent.children(".closetabs").addClass("remove");
+        $parent.children(".palise-mini-circ").addClass("small");
+        $parent.children(".palise-mini-grad").addClass("fade");
     });
 
-    $(".mini1").on("click", function () {
-        $(this).parents("#mini-out").children(".mini1").addClass('clicked');
-        $(this).parents("#mini-out").children(".tab1").addClass('show');
-        $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-        $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-        $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-    });
-    $(".mini2").on("click", function () {
-        $(this).parents("#mini-out").children(".mini2").addClass('clicked');
-        $(this).parents("#mini-out").children(".tab2").addClass('show');
-        $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-        $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-        $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-    });
-    $(".mini3").on("click", function () {
-        $(this).parents("#mini-out").children(".mini3").addClass('clicked');
-        $(this).parents("#mini-out").children(".tab3").addClass('show');
-        $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-        $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-        $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-    });
-    $(".mini4").on("click", function () {
-        $(this).parents("#mini-out").children(".mini4").addClass('clicked');
-        $(this).parents("#mini-out").children(".tab4").addClass('show');
-        $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-        $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-        $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-    });
-    $(".mini5").on("click", function () {
-        $(this).parents("#mini-out").children(".mini5").addClass('clicked');
-        $(this).parents("#mini-out").children(".tab5").addClass('show');
-        $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-        $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-        $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-    });
-    $(".mini6").on("click", function () {
-        $(this).parents("#mini-out").children(".mini6").addClass('clicked');
-        $(this).parents("#mini-out").children(".tab6").addClass('show');
-        $(this).parents("#mini-out").children(".closetabs").removeClass("remove");
-        $(this).parents("#mini-out").children(".palise-mini-circ").removeClass("small");
-        $(this).parents("#mini-out").children(".palise-mini-grad").removeClass("fade");
-    });
+    // Handle mini1 → mini6 clicks dynamically
+    for (let i = 1; i <= 6; i++) {
+        $(`#mini-out .mini${i}`).off('click').on('click', function () {
+            const $parent = $(this).parents("#mini-out");
+
+            // First reset all
+            $parent.children(".mini").removeClass('clicked');
+            $parent.children(".tab").removeClass('show');
+            $parent.children(".closetabs").addClass("remove");
+            $parent.children(".palise-mini-circ").addClass("small");
+            $parent.children(".palise-mini-grad").addClass("fade");
+
+            // Then activate this mini/tab
+            $parent.children(`.mini${i}`).addClass('clicked');
+            $parent.children(`.tab${i}`).addClass('show');
+            $parent.children(".closetabs").removeClass("remove");
+            $parent.children(".palise-mini-circ").removeClass("small");
+            $parent.children(".palise-mini-grad").removeClass("fade");
+        });
+    }
+}
+
+// Initialize on page load
+initMiniTabs();
+
+// Re-initialize after any AJAX load
+$(document).ajaxComplete(function () {
+    initMiniTabs();
 });
 
+// ----- Palise side toggle buttons (no change needed) -----
 $(".palise-clik-1").on("click", function () {
     let $parent = $(this).parents(".palise-side-2");
     $parent.children(".palise-side-toggle-1").addClass('toggle-on');
     $parent.children(".palise-side-toggle-2, .palise-side-toggle-3, .palise-side-toggle-4").removeClass('toggle-on');
 });
-
 $(".palise-clik-2").on("click", function () {
     let $parent = $(this).parents(".palise-side-2");
     $parent.children(".palise-side-toggle-2").addClass('toggle-on');
     $parent.children(".palise-side-toggle-1, .palise-side-toggle-3, .palise-side-toggle-4").removeClass('toggle-on');
 });
-
 $(".palise-clik-3").on("click", function () {
     let $parent = $(this).parents(".palise-side-2");
     $parent.children(".palise-side-toggle-3").addClass('toggle-on');
     $parent.children(".palise-side-toggle-1, .palise-side-toggle-2, .palise-side-toggle-4").removeClass('toggle-on');
 });
-
 $(".palise-clik-4").on("click", function () {
     let $parent = $(this).parents(".palise-side-2");
     $parent.children(".palise-side-toggle-4").addClass('toggle-on');
     $parent.children(".palise-side-toggle-1, .palise-side-toggle-2, .palise-side-toggle-3").removeClass('toggle-on');
 });
-
-// Toggle-off buttons (combined into one)
 $(".palise-side-toggle-off").on("click", function () {
     let $parent = $(this).parents(".palise-side-2");
     $parent.children(".palise-side-toggle-1, .palise-side-toggle-2, .palise-side-toggle-3, .palise-side-toggle-4").removeClass('toggle-on');
 });
+
 
