407240510933499ad59ba75bbbd8c5e449d0852a
Hoenn-RP <159826511+Hoenn-RP@users.noreply.github.com>
2026-04-05 12:00:14 -0400

    Update eggroll.js

--- eggroll.js
+++ eggroll.js
@@ -19,69 +19,151 @@ function EggrollDice() {
 
         let resultText = '';
 
-        // Tier 1: 1–25
-        if (finalRoll >= 1 && finalRoll <= 25) {
-            resultText = `
-                <div class="mkj-wrapper">
-                    <div class="mkj-textbox">
-                        <b>(${finalRoll})</b> Tier 1 Result<br>
-                        Placeholder effect for rolls 1–25.
-                    </div>
-                </div>`;
+        // -----------------------------
+        // eggroll-1 → Moves
+        // -----------------------------
+        if ($(this).closest('.eggroll').hasClass('eggroll-1')) {
+
+            if (finalRoll <= 20) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg grants <b>1 TM Move</b>!</div>`;
+            } else if (finalRoll <= 40) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg grants <b>1 Egg Move</b>!</div>`;
+            } else if (finalRoll <= 60) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg grants <b>2 Egg Moves</b>!</div>`;
+            } else if (finalRoll <= 80) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg grants <b>1 Event Move</b>!</div>`;
+            } else { // 81–120
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg grants <b>Wish Move</b>!</div>`;
+            }
+
         }
 
-        // Tier 2: 26–50
-        else if (finalRoll <= 50) {
-            resultText = `
-                <div class="mkj-wrapper">
-                    <div class="mkj-textbox">
-                        <b>(${finalRoll})</b> Tier 2 Result<br>
-                        Placeholder effect for rolls 26–50.
-                    </div>
-                </div>`;
+        // -----------------------------
+        // eggroll-2 → Effects
+        // -----------------------------
+        else if ($(this).closest('.eggroll').hasClass('eggroll-2')) {
+
+            if (finalRoll <= 20) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg has <b>Hidden Ability</b>!</div>`;
+            } else if (finalRoll <= 40) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg replaces ability with <b>Cute Charm</b>!</div>`;
+            } else if (finalRoll <= 60) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg hatches a <b>Shiny</b> Pokémon!</div>`;
+            } else if (finalRoll <= 80) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg hatches a <b>Pink</b> Pokémon!</div>`;
+            } else if (finalRoll <= 100) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg hatches an <b>Illumina Pokémon</b>!</div>`;
+            } else if (finalRoll <= 110) {
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg grants an <b>Extra Moveslot</b>!</div>`;
+            } else { // 111–120
+                resultText = `
+                <img src="https://i.imgur.com/bwjRsaA.png" title="Pokemon Egg">
+                <div class="cramomatic-desc">This egg grants an <b>Extra Ability Slot</b>!</div>`;
+            }
+
         }
 
-        // Tier 3: 51–75
-        else if (finalRoll <= 75) {
-            resultText = `
-                <div class="mkj-wrapper">
-                    <div class="mkj-textbox">
-                        <b>(${finalRoll})</b> Tier 3 Result<br>
-                        Placeholder effect for rolls 51–75.
-                    </div>
-                </div>`;
+        // -----------------------------
+        // eggroll-3 → Species
+        // -----------------------------
+        else if ($(this).closest('.eggroll').hasClass('eggroll-3')) {
+
+            if (finalRoll <= 4) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Bunnelby Egg"><div class="cramomatic-desc">The egg hatches into <b>Bunnelby</b>!</div>`;
+            } else if (finalRoll <= 8) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Cherubi Egg"><div class="cramomatic-desc">The egg hatches into <b>Cherubi</b>!</div>`;
+            } else if (finalRoll <= 12) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Tynamo Egg"><div class="cramomatic-desc">The egg hatches into <b>Tynamo</b>!</div>`;
+            } else if (finalRoll <= 16) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Psyduck Egg"><div class="cramomatic-desc">The egg hatches into <b>Psyduck</b>!</div>`;
+            } else if (finalRoll <= 20) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Shroomish Egg"><div class="cramomatic-desc">The egg hatches into <b>Shroomish</b>!</div>`;
+            } else if (finalRoll <= 24) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Togepi Egg"><div class="cramomatic-desc">The egg hatches into <b>Togepi</b>!</div>`;
+            } else if (finalRoll <= 28) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Tyrogue Egg"><div class="cramomatic-desc">The egg hatches into <b>Tyrogue</b>!</div>`;
+            } else if (finalRoll <= 32) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Smoochum Egg"><div class="cramomatic-desc">The egg hatches into <b>Smoochum</b>!</div>`;
+            } else if (finalRoll <= 36) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Elekid Egg"><div class="cramomatic-desc">The egg hatches into <b>Elekid</b>!</div>`;
+            } else if (finalRoll <= 40) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Magby Egg"><div class="cramomatic-desc">The egg hatches into <b>Magby</b>!</div>`;
+            } else if (finalRoll <= 44) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Cleffa Egg"><div class="cramomatic-desc">The egg hatches into <b>Cleffa</b>!</div>`;
+            } else if (finalRoll <= 48) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Igglybuff Egg"><div class="cramomatic-desc">The egg hatches into <b>Igglybuff</b>!</div>`;
+            } else if (finalRoll <= 52) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Azurill Egg"><div class="cramomatic-desc">The egg hatches into <b>Azurill</b>!</div>`;
+            } else if (finalRoll <= 56) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Wynaut Egg"><div class="cramomatic-desc">The egg hatches into <b>Wynaut</b>!</div>`;
+            } else if (finalRoll <= 60) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Budew Egg"><div class="cramomatic-desc">The egg hatches into <b>Budew</b>!</div>`;
+            } else if (finalRoll <= 64) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Chingling Egg"><div class="cramomatic-desc">The egg hatches into <b>Chingling</b>!</div>`;
+            } else if (finalRoll <= 68) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Bonsly Egg"><div class="cramomatic-desc">The egg hatches into <b>Bonsly</b>!</div>`;
+            } else if (finalRoll <= 72) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Mime Jr. Egg"><div class="cramomatic-desc">The egg hatches into <b>Mime Jr.</b>!</div>`;
+            } else if (finalRoll <= 76) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Happiny Egg"><div class="cramomatic-desc">The egg hatches into <b>Happiny</b>!</div>`;
+            } else if (finalRoll <= 80) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Munchlax Egg"><div class="cramomatic-desc">The egg hatches into <b>Munchlax</b>!</div>`;
+            } else if (finalRoll <= 84) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Mantyke Egg"><div class="cramomatic-desc">The egg hatches into <b>Mantyke</b>!</div>`;
+            } else if (finalRoll <= 88) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Toxel Egg"><div class="cramomatic-desc">The egg hatches into <b>Toxel</b>!</div>`;
+            } else if (finalRoll <= 92) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Skitty Egg"><div class="cramomatic-desc">The egg hatches into <b>Skitty</b>!</div>`;
+            } else if (finalRoll <= 96) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Buneary Egg"><div class="cramomatic-desc">The egg hatches into <b>Buneary</b>!</div>`;
+            } else if (finalRoll <= 100) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Drifloon Egg"><div class="cramomatic-desc">The egg hatches into <b>Drifloon</b>!</div>`;
+            } else if (finalRoll <= 104) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Teddiursa Egg"><div class="cramomatic-desc">The egg hatches into <b>Teddiursa</b>!</div>`;
+            } else if (finalRoll <= 108) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Torchic Egg"><div class="cramomatic-desc">The egg hatches into <b>Torchic</b>!</div>`;
+            } else if (finalRoll <= 112) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Riolu Egg"><div class="cramomatic-desc">The egg hatches into <b>Riolu</b>!</div>`;
+            } else if (finalRoll <= 116) {
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Pichu Egg"><div class="cramomatic-desc">The egg hatches into <b>Pichu</b>!</div>`;
+            } else { // 117–120
+                resultText = `<img src="https://i.imgur.com/bwjRsaA.png" title="Eevee Egg"><div class="cramomatic-desc">The egg hatches into <b>Eevee</b>!</div>`;
+            }
+
         }
 
-        // Tier 4: 76–100
-        else if (finalRoll <= 100) {
-            resultText = `
-                <div class="mkj-wrapper">
-                    <div class="mkj-textbox">
-                        <b>(${finalRoll})</b> Tier 4 Result<br>
-                        Placeholder effect for rolls 76–100.
-                    </div>
-                </div>`;
-        }
-
-        // Tier 5: 101–110
-        else if (finalRoll <= 120) {
-            resultText = `
-                <div class="mkj-wrapper">
-                    <div class="mkj-textbox">
-                        <b>(${finalRoll})</b> Tier 5 Result ⭐<br>
-                        High-tier result for rolls 101–110.
-                    </div>
-                </div>`;
-        }
-
+        // -----------------------------
         // Fallback if something goes wrong
+        // -----------------------------
         else {
             resultText = `
-                <div class="mkj-wrapper">
-                    <div class="mkj-textbox">
-                        (${finalRoll}) Something went wrong...
-                    </div>
-                </div>`;
+            <div class="mkj-wrapper">
+                <div class="mkj-textbox">
+                    (${finalRoll}) Something went wrong...
+                </div>
+            </div>`;
         }
 
         // Replace the original roll with the result HTML
@@ -101,7 +183,6 @@ $(document).ready(function () {
         const observer = new MutationObserver(() => {
             EggrollDice();
         });
-
         observer.observe(target, { childList: true, subtree: true });
     }
 });
